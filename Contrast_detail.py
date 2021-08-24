9184990f0cceafeb7179887d56899535d4b8969d
Elena Cantoni <cantonielena.97@gmail.com>
2021-08-24 19:50:52 +0200

    Add a for loop to exploit more general variables for runnig the same code with different datasets
    In the previous commit a large number of .txt and .csv files are added in "data" directory and are used to run the codes and perform the analysis.

--- Contrast_detail.py
+++ Contrast_detail.py
@@ -11,19 +11,21 @@ txt_acq_95_22_med = "~/cd_acq_95_22_med.txt"
 txt_FL_LAAG_0001 = "~/cd_FL_LAAG_0001.txt"
 txt_FL_LAAG_0002 = "~/cd_FL_LAAG_0002.txt"
 txt_CORO_LAAG_0003 = "~/cd_CORO_LAAG_0003.txt"
+txt_CORO_LAAG_0004 = "~/cd_CORO_LAAG_0004.txt"
 #txt_acq14_5_12 = "C://Users/canto/Google Drive/UNIBO MAGISTRALE_/Software and computing for applied physics/CHOanalysis/cd_acq14_5_12.txt"
 
 #change the following path with the folder local path on your personal computer
 local_path = "C://Users/canto/Google Drive/UNIBO MAGISTRALE_/Software and computing for applied physics/CHOanalysis/data"
-
-files = pd.read_csv(txt_FL_LAAG_0002, delimiter = "=")
+#local_path1 = "C://Users/canto/Google Drive/UNIBO MAGISTRALE_/Software and computing for applied physics/CHOanalysis"
+os.environ["HOME"] = local_path
+files = pd.read_csv(txt_CORO_LAAG_0004, delimiter = "=")
 path = [files['path']]
 path = path[0]
 
 for n_p in range(0,len(path)):
     p = os.path.expanduser(path[n_p])
     
-    os.environ["HOME"] = local_path
+    #os.environ["HOME"] = local_path
     p = os.path.expanduser(path[n_p])
     files['path'][n_p] = p
 
@@ -52,10 +54,19 @@ for p in range(0,len(files['path'])):
 df_alpha = pd.DataFrame(m_contr, columns = path_s) 
     
 # plotting
+for a in range(0,len(path_s)-1):
+    if "alpha" in path_s[a]:
+        a_val = path_s[a]
+        a_val = a
+
 fig,ax = plt.subplots(figsize=(12,9))
-for c_d in path_s[1:-1]:
+for c_d in path_s[1:a_val+1]:# -1
     ax.plot(df_alpha['diam'],df_alpha[c_d],'--o')
-ax.plot(df_alpha['diam'],df_alpha['human curve'],'r-o', lw = 2.5)
+lin_col = ['red','blue','green','yellow']
+colors = -1
+for c_d_h in path_s[(a_val+1):]:
+    colors +=1
+    ax.plot(df_alpha['diam'],df_alpha[c_d_h],c = lin_col[colors],marker='o',linestyle='-', linewidth=3)
 ax.set_title('Contrast-detail curve', fontsize=16,)
 ax.set_xlabel('Diameter (mm)',fontsize=12)
 ax.set_ylabel('Contrast',fontsize=12)
