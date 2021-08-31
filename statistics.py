89ce85d09553412a2450d7caec0ebec009b8258b
Elena Cantoni <cantonielena.97@gmail.com>
2021-08-31 17:19:11 +0200

    Generate a 3D matrix 'm_correlation' including every result of 'correlation' function of each CHO curve for each observer.
    The correlation is studied between each observer curve and CHO alpha-dependent curve.
    An overview about slope, intercept, R value, p value and uncertaintiesis of each relation is given.

--- statistics.py
+++ statistics.py
@@ -6,9 +6,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
 from scipy.stats import linregress
-from Contrast_detail import files,df_alpha,a_val
-from minimization import df_sum_w_dist
-
+from Contrast_detail import *#files,df_alpha,a_val,path_s,alpha_s,human_s
+from minimization import protocol_curvemin#files, a_val,sum_w_dist,df_sum_w_dist
+path_interaction = 'C://Users/canto/Google Drive/UNIBO MAGISTRALE_/Software and computing for applied physics/CHOanalysis'
+sum_w_dist_pkl = pd.read_pickle(path_interaction + '/min_dist.pkl')    
+df_protocol_curvemin_pkl = pd.read_pickle(path_interaction + '/protocol_curvemin.pkl')
 #studying correlation between human and CHO model
 def correlation(ref, alphas):
     """
@@ -23,28 +25,28 @@ def correlation(ref, alphas):
     Returns
     -------
     slope : Slope of the regression line
-    intercept : TYPE
-        DESCRIPTION.
-    r_value : TYPE
-        DESCRIPTION.
-    std_err : TYPE
-        DESCRIPTION.
+    intercept : Intercept of the regression line
+    r_value : Correlation factor
+    std_err : Standard Deviation of the estimated slope
 
     """
     r_corr = pearsonr(ref,alphas)
     slope, intercept, r_value, p_value, std_err = linregress(alphas,ref)
-    return  slope, intercept,r_value,p_value,std_err
+    return  slope, intercept,r_value,p_value,std_err       
+        
 
-
-m_correlation = np.ndarray((5,a_val))
+m_correlation = np.ndarray((((len(files['alpha'])-a_val)),5,a_val))
 alpha_s = np.ndarray((a_val,),object)
-for a in range(0,a_val):
-    alpha = files['alpha'][a]
-    col_alpha = df_alpha[alpha]
-    alpha_s[a] = alpha
-    corr = correlation(df_alpha['human curve'], col_alpha)
-    corr = np.array(corr)
-    print(corr)
-    m_correlation[:,a] = corr
-df_correlation = pd.DataFrame(m_correlation, columns = alpha_s, 
-                              index = ['slope','intercept','r value','p value','std_err'])
+hum = -1
+for human in np.array(files['alpha'][a_val:]):
+    hum +=1
+    #print(hum)
+    for a in range(0,a_val):
+        alpha = files['alpha'][a]
+        col_alpha = df_alpha[alpha]
+        alpha_s[a] = alpha
+        corr = correlation(df_alpha[human], col_alpha)
+        #corr = np.array(corr)
+        #print(corr)
+        m_correlation[hum,:,a] = corr
+
